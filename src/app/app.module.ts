f6f9f0d3a891cbda1ab21786e11dffa34ee17433
Victor Medeiros <jvleitao@stefanini.com>
2018-05-16 17:07:15 -0300

    Navegação de Rotas Filhas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+
 
 
 
@@ -30,7 +32,8 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
     RestaurantDetailComponent,
     MenuComponent,
     ShoppingCartComponent,
-    MenuItemComponent
+    MenuItemComponent,
+    ReviewsComponent
 
 
   ],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,21 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
 import { Routes } from "@angular/router";
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
 
 export const ROUTES: Routes = [
-    { path: '', component: HomeComponent },
+    //Navegação 'nome da rota', Componente
+    { path: '', component: HomeComponent },//redireciona para a página Home
     { path: 'restaurants', component: RestaurantsComponent },
-    { path: 'restaurants/:id', component: RestaurantDetailComponent },
+    {
+        path: 'restaurants/:id', component: RestaurantDetailComponent,
+        //Navegação 'componente pai/componente filho', Componente
+        children: [
+            { path: '', redirectTo: 'menu', pathMatch: 'full' },//redireciona para o menu
+            { path: 'menu', component: MenuComponent },
+            { path: 'reviews', component: ReviewsComponent }
+        ]
+    },
     { path: 'about', component: AboutComponent }
 ]
